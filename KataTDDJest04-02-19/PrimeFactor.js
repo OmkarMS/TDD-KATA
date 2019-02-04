4e0c70273d1810b6522dc28ec9daccdda8f86791
OmkarMS <omkar.sahasrabudhe@synerzip.comclear>
2019-02-04 08:56:28 +0530

    Kata 04-02-19

---
+++ KataTDDJest04-02-19/PrimeFactor.js
@@ -0,0 +1,17 @@
+exports.PrimeFactor = (number) => {
+    pFact = []
+    if (number == 1) {
+        return null
+    }
+
+    fact = 2
+    while (number >= fact) {
+        if (number % fact == 0) {
+            pFact.push(fact)
+            number = number / fact
+        } else
+            fact++
+    }
+
+    return pFact
+}
